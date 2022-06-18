9be149173c50d683df4a26f1749adc5bcc71c7a6
CQL02 <chungqilin02@gmail.com>
2022-06-18 20:18:36 +0800

    added all style so far no bug

--- pages/Driver/resign.js
+++ pages/Driver/resign.js
@@ -1,26 +1,17 @@
-import Head from 'next/head'
-import DriverResignComponent from "../../Component/DriverResignComponent"
+import Head from "next/head";
+import DriverResignComponent from "../../Component/DriverResignComponent";
 
- 
 function resign() {
-    return (
-        <div style={{backgroundImage: "url(../driver-bg.jpg)", backgroundSize: "cover", height: "100vh"}}>
-            
-            <Head>
-                <title>Driver</title>
-                <link rel="icon" href="/pupg-icon.ico" />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>Driver</title>
+        <link rel="icon" href="/pupg-icon.ico" />
+      </Head>
 
-            <div style={{
-                position: "absolute", 
-                top: "50%", 
-                left: "50%",
-                transform: "translate(-50%, -50%)"
-                }}>
-                <DriverResignComponent />
-            </div>
-        </div>
-    )
+      <DriverResignComponent />
+    </div>
+  );
 }
 
-export default resign
+export default resign;
